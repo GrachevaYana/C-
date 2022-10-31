b72633e2520df1d0172a7a2998e0466de63a5091
Yana_Gracheva <push1022@ya.ru>
2022-10-31 21:50:14 +0300

    ДЗ по семинару 1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет, дорогой друг!");
+Console.WriteLine("Давай проверим является ли одно число квадратом другого.");
+Console.WriteLine("Введи первое число: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введи второе число, которое надо проверить является ли квадратом первого числа: ");
+int b = Convert.ToInt32(Console.ReadLine()!);
+int result = a * a;
+if (result == b)
+{
+    Console.WriteLine($"Второе число {b} является квадратом первого числа {a}");
+}
+else
+{
+    Console.WriteLine($"Второе число {b} не является квадратом первого числа {a}");
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 2: Напишите программу, которая на вход принимает два числа и
+//выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7->max = 7
+//a = 2 b = 10->max = 10
+//a = -9 b = -3->max = -3
+Console.WriteLine("Привет, дорогой друг!");
+Console.WriteLine("Давай помогу тебе узнать какое из двух числе больше.");
+Console.WriteLine("Введи первое число: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введи второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()!);
+if (a > b)
+{ Console.WriteLine("Первое число " + a + " больше второго числа " + b); }
+else if (a == b)
+{ Console.WriteLine("Введенные числа равны"); }
+else 
+{ Console.WriteLine("Второе число " + b + " больше первого числа " + a); }
